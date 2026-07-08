e8de38baf8e3055dee1751dadf781a5c4452fffe
Joesph Pettigrew <muttpettigrew@gmail.com>
2026-07-07 23:15:19 -0600

    Create initial React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#111827",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>ManaVault</h1>
+
+      <h2>Inventory & Advanced Search for Magic: The Gathering</h2>
+
+      <p>Welcome to Version 0.1 🚀</p>
+    </div>
+  );
+}
+
+export default App;
